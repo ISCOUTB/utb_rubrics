9b62fb002cf71cc4b03d40471df84a3219437cb3
IsaacDev2024 <isaacestudia123@gmail.com>
2025-10-23 14:58:10 -0500

    Update version to 1.1.0

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * UTB Rubrics grading method version file
- * Version 1.0.7 - Production Ready
+ * Version 1.1.0 - Production Ready
  *
  * @package    gradingform_utbrubrics
  * @copyright  2025 Isaac Sanchez, Santiago Orejuela, Luis Diaz, Maria Valentina
@@ -11,8 +11,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'gradingform_utbrubrics';
-$plugin->version = 2025102101; // Version: YYYYMMDDHH format - Version 1.0.7 Official Release
+$plugin->version = 2025102301; // Version: YYYYMMDDHH format - Version 1.1.0 Official Release
 $plugin->requires  = 2022041900; // Requires Moodle 4.0 or higher (2022041900)
 $plugin->maturity  = MATURITY_STABLE; // Production ready - Official Release
-$plugin->release = '1.0.7'; // Official Release
+$plugin->release = '1.1.0'; // Official Release
 $plugin->dependencies = array();
